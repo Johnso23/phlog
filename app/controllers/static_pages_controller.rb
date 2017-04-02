8c6d74d1ac280db039a9e74cb869394a70ad943e
Johnso23 <John.s.occhipinti@gmail.com>
2017-04-02 03:35:39 -0400

    implement basic login

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,28 @@
-class StaticPagesController < ApplicationController
+class UsersController < ApplicationController
 
-  def home
+  def show
+    @user = User.find(params[:id])
   end
 
-  def help
+  def new
+    @user = User.new
   end
 
-  def about
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-  def contact
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
